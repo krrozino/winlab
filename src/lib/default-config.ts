e7c1e352256bc8bc0b87891b0ec19bfe34040886
Sergio Izaque Pinheiro Carrozino <129542135+krrozino@users.noreply.github.com>
2026-09-17 22:54:34 -0300

    feat: update src/lib/default-config.ts for mvp 0.2

--- src/lib/default-config.ts
+++ src/lib/default-config.ts
@@ -4,8 +4,9 @@ export const defaultConfig: Config = {
   profileName: "Laboratório Escolar",
   studentUser: "Aluno",
   adminUser: "MicrolinsAdmin",
-
   createAccounts: true,
+
+  enforcementMode: "AuditOnly",
 
   blockInstallers: true,
   blockStoreApps: true,
@@ -22,6 +23,7 @@ export const defaultConfig: Config = {
   blockWallpaper: true,
   blockMousePointers: true,
   blockSoundScheme: true,
+  wallpaperUnlockMinutes: 90,
 
   allowedApps: ["chrome", "word", "excel", "powerpoint", "powerbi"],
   customAllowedPaths: []
